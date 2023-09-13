24ef84e1d8efcafb8d34249095a14c4f2f485330
Martin Endler <pokusew@seznam.cz>
2023-09-13 03:58:23 +0200

    Fix unified related TypeScript type errors

--- app/text/format.tsx
+++ app/text/format.tsx
@@ -11,10 +11,17 @@ import rehypeStringify from 'rehype-stringify';
 
 export const processTextToHtml = (input: string): string => {
 
+	// TODO: remove ts-ignore comments once all unified libs are ready
+	//       see https://github.com/unifiedjs/unified/issues/228
+	//       see https://github.com/unifiedjs/unified/issues/227
 	const file = unified()
+		// @ts-ignore
 		.use(remarkParse)
+		// @ts-ignore
 		.use(remarkMath)
+		// @ts-ignore
 		.use(remarkRehype)
+		// @ts-ignore
 		.use(rehypeKatex)
 		.use(rehypeStringify)
 		.processSync(input);
